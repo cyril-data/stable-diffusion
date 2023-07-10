e272d1e35caf3e4e17e348b340a4128b39bd1dfe
cyril-data <cyrilregan@mail.com>
2023-07-10 10:53:09 +0200

    plot scheduler

--- trash_scheduler_lr.py
+++ trash_scheduler_lr.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 
 LEARNING_RATE = 1e-6
-EPOCHS = 100
-STEPS_IN_EPOCH = 25
+EPOCHS = 400
+STEPS_IN_EPOCH = 255
 
 print("STEPS_IN_EPOCH", STEPS_IN_EPOCH)
 
@@ -17,15 +17,25 @@ optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
 
 # Define your scheduler here as described above
 
-base_lr = LEARNING_RATE/10
-max_lr = LEARNING_RATE
+# base_lr = LEARNING_RATE/10
+# max_lr = LEARNING_RATE
+# scheduler = lr_scheduler.CyclicLR(optimizer,
+#                                   base_lr=base_lr,  # Initial learning rate which is the lower boundary in the cycle for each parameter group
+#                                   max_lr=max_lr,  # Upper learning rate boundaries in the cycle for each parameter group
+#                                   # Number of training iterations in the increasing half of a cycle
+#                                   step_size_up=STEPS_IN_EPOCH * 5,
+#                                   mode="triangular2")
 
-scheduler = lr_scheduler.CyclicLR(optimizer,
-                                  base_lr=base_lr,  # Initial learning rate which is the lower boundary in the cycle for each parameter group
-                                  max_lr=max_lr,  # Upper learning rate boundaries in the cycle for each parameter group
-                                  # Number of training iterations in the increasing half of a cycle
-                                  step_size_up=STEPS_IN_EPOCH * 5,
-                                  mode="triangular2")
+
+max_lr = LEARNING_RATE
+scheduler = lr_scheduler.OneCycleLR(optimizer,
+                                    max_lr=max_lr,  # Upper learning rate boundaries in the cycle for each parameter group
+                                    # The number of steps per epoch to train for.
+                                    steps_per_epoch=int(260),
+                                    # The number of epochs to train for.
+                                    epochs=EPOCHS,
+                                    anneal_strategy='cos')  # Specifies the annealing strategy
+
 
 # Get learning rates as each training step
 steps = []
@@ -33,7 +43,7 @@ learning_rates = []
 
 for i in range(EPOCHS*STEPS_IN_EPOCH):
     optimizer.step()
-    if i % 10 == 0:
+    if i % 1000 == 0:
         steps.append(i)
         learning_rates.append(optimizer.param_groups[0]["lr"])
     scheduler.step()
